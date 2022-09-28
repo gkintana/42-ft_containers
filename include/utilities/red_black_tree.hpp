008224d20beb10a945a22b9cf104432b4b29bd05
gkintana <81775867+gkintana@users.noreply.github.com>
2022-09-29 00:38:57 +0400

    added red black tree resources

--- include/utilities/red_black_tree.hpp
+++ include/utilities/red_black_tree.hpp
@@ -6,13 +6,31 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:39:21 by gkintana          #+#    #+#             */
-/*   Updated: 2022/09/26 22:39:37 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/09/29 00:36:05 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RED_BLACK_TREE_HPP
 #define RED_BLACK_TREE_HPP
 
+// https://github.com/llvm/llvm-project/tree/main/libcxx
+// https://github.com/gcc-mirror/gcc/tree/master/libstdc%2B%2B-v3/include/bits
+// https://www.happycoders.eu/algorithms/red-black-tree-java/
+// https://www.cs.auckland.ac.nz/software/AlgAnim/red_black.html
+// https://web.stanford.edu/class/archive/cs/cs106l/cs106l.1176/lectures/lecture05/05_Templates.pdf
+// https://web.stanford.edu/class/archive/cs/cs106l/cs106l.1176/lectures/
 
+enum tree_color { RED = 1, BLACK = 0 };
 
-#endif
+struct node {
+	node *parent;
+	node *child[2];
+	enum tree_color color;
+	int key;
+};
+
+struct tree {
+	node *root;
+};
+
+#endif    // RED_BLACK_TREE_HPP
