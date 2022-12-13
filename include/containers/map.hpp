bb267452914f6e10b5ad6f1e48fdf080fca259c0
gkintana <81775867+gkintana@users.noreply.github.com>
2022-12-14 00:15:08 +0400

    map: added missing swap specialization & update typedefs

--- include/containers/map.hpp
+++ include/containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 17:02:49 by gkintana          #+#    #+#             */
-/*   Updated: 2022/12/13 23:02:07 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/12/14 00:06:17 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,12 @@ class map {
 		typedef ft::pair<const key_type, mapped_type>          value_type;
 		typedef Compare                                        key_compare;
 		typedef Allocator                                      allocator_type;
-		// typedef typename allocator_type::reference          reference;
-		// typedef typename allocator_type::const_reference    const_reference;
-		// typedef typename allocator_type::pointer            pointer;
-		// typedef typename allocator_type::const_pointer      const_pointer;
+		typedef typename allocator_type::reference             reference;
+		typedef typename allocator_type::const_reference       const_reference;
+		typedef typename allocator_type::pointer               pointer;
+		typedef typename allocator_type::const_pointer         const_pointer;
 		typedef typename allocator_type::size_type             size_type;
 		typedef typename allocator_type::difference_type       difference_type;
-
-		typedef tree_node<value_type>                          node_type;
-		typedef node_type*                                     pointer;
 		typedef avl_tree<key_type, mapped_type,
 		                 key_compare, allocator_type>          tree_type;
 		typedef map_iterator<value_type, key_compare>          iterator;
@@ -356,6 +353,11 @@ bool operator>(const ft::map<Key, T, Compare, Allocator> &lhs, const ft::map<Key
 template <class Key, class T, class Compare, class Allocator>
 bool operator>=(const ft::map<Key, T, Compare, Allocator> &lhs, const ft::map<Key, T, Compare, Allocator> &rhs) {
 	return !(lhs < rhs);
+}
+
+template <class Key, class T, class Compare, class Allocator>
+void swap(ft::map<Key, T, Compare, Allocator> &lhs, ft::map<Key, T, Compare, Allocator> &rhs) {
+	lhs.swap(rhs);
 }
 
 }    // namespace ft
